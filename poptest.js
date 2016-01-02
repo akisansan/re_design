ee4385abc6cf6ae9bd496aff2791c621b509e676
akisansan <b.ra2.atu@gmail.com>
2016-01-02 16:23:52 +0900

    poptest 時間取得のコメントアウト

--- poptest.js
+++ poptest.js
@@ -9,7 +9,7 @@ function notifyMe() {
   // すでに通知の許可を得ているか確認する
   else if (Notification.permission === "granted") {
     // 許可を得ている場合は、通知を作成する
-    var notification = new Notification("Hi there!"+gTimeStart);
+    var notification = new Notification("Hi there!");
     notification.addEventListener('click', function() {
       console.log("hey");
       open('https://www.google.co.jp/');
@@ -21,7 +21,7 @@ function notifyMe() {
     Notification.requestPermission(function (permission) {
       // ユーザが許可した場合は、通知を作成する
       if (permission === "granted") {
-        var notification = new Notification("Hi there!"+ gTimeStart);
+        var notification = new Notification("Hi there!");
         // クリックでWebページを開く
         notification.addEventListener('click', function() {
           console.log("hey");
@@ -32,10 +32,10 @@ function notifyMe() {
   }
   setTimeout('notifyMe();',1000);
 }
-
+/*
   var dd = new Date();    // 開始時間取得
   var gTimeStart = dd.getTime();
   gTimeStart= gTimeStart/(1000*60);
   console.log(gTimeStart);
-
+  */
 setTimeout('notifyMe();',1000);
