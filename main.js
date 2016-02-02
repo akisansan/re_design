61b7556dc8b2cd6c983498dd491ffc456b1bda83
akisansan <b.ra2.atu@gmail.com>
2016-02-02 13:33:31 +0900

    poptest.jsが通知＆Json取得

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ app.on('window-all-closed', function() {
 	});
 
 app.on('ready', function() {
-		mainWindow = new BrowserWindow({width: 800, height: 600});
+		mainWindow = new BrowserWindow({width: 1800, height: 1200});
 		mainWindow.loadUrl('file://' + __dirname + '/index.html');
 		mainWindow.on('closed', function() {
 				mainWindow = null;
--- poptest.js
+++ poptest.js
@@ -1,39 +1,48 @@
+var temperature = 0;        //温度
+var humidity = 0;           //湿度
+var pressure = 0;           //気圧
+
 function notifyMe() {
-  // ブラウザが通知をサポートしているか確認する
-  if (!("Notification" in window)) {
-    alert("このブラウザはシステム通知をサポートしていません");
-  }
+  $.getJSON("http://192.168.1.172/redesign/humidity/env_data", function(data) {
+    temperature = data.temperature;
+    humidity = data.humidity;
+    pressure = data.pressure;
+  });
 
-  // すでに通知の許可を得ているか確認する
-  else if (Notification.permission === "granted") {
-    // 許可を得ている場合は、通知を作成する
-    var notification = new Notification("Hi there!");
-    notification.addEventListener('click', function() {
-      console.log("hey");
-      open('https://www.google.co.jp/');
-    });
-  }
+  //閾値以上になった場合に通知作成
+  if (humidity > 30.0) {
+    if (!("Notification" in window)) {
+      alert("このブラウザはシステム通知をサポートしていません");
+    }
 
-  // 許可を得ていない場合は、ユーザに許可を求めなければならない
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // ユーザが許可した場合は、通知を作成する
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-        // クリックでWebページを開く
-        notification.addEventListener('click', function() {
-          console.log("hey");
-          open('https://www.google.co.jp/');
-        });
-      }
-    });
+    // すでに通知の許可を得ているか確認する
+    else if (Notification.permission === "granted") {
+      // 許可を得ている場合は、通知を作成する
+      var notification = new Notification("Hi there!" + humidity);//Notification以下が通知内容
+      notification.addEventListener('click', function() {
+        console.log("hey");
+        //  window.open('https://www.google.co.jp/');
+        location.href = "https://www.google.co.jp";//通知バークリック時のジャンプ先
+      });
+    }
+
+    // 許可を得ていない場合は、ユーザに許可を求めなければならない
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function(permission) {
+        // ユーザが許可した場合は、通知を作成する
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!" + humidity);
+          // クリックでWebページを開く
+          notification.addEventListener('click', function() {
+            console.log("hey");
+            //  window.open('https://www.google.co.jp/','home');
+            location.href = "https://www.google.co.jp";
+          });
+        }
+      });
+    }
   }
-  setTimeout('notifyMe();',60000);//1分ごと
 }
-/*
-  var dd = new Date();    // 開始時間取得
-  var gTimeStart = dd.getTime();
-  gTimeStart= gTimeStart/(1000*60);
-  console.log(gTimeStart);
-  */
-setTimeout('notifyMe();',60000);
+
+
+timerID = setInterval('notifyMe();' /*定期的に呼び出す関数名*/ , 2000 /*呼び出す間隔*/ );
