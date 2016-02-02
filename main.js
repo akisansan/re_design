ee82fc1114ee35b24b68bf7f834b5b8c1c3d4b5b
akisansan <b.ra2.atu@gmail.com>
2016-02-02 23:31:57 +0900

    整理したんだよぉ

--- main.js
+++ main.js
@@ -1,51 +1,3 @@
-/*'use strict';
-
-var app = require('app');
-var BrowserWindow = require('browser-window');
-require('crash-reporter').start();
-var mainWindow = null;
-var willAppQuit = false;
-
-app.on('closed', function() {
-  if (process.platform != 'win32')
-  console.log("close");
-    app.quit();
-});
-
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  mainWindow.on('close', function(event){
-    // Minimize or hide the window for close button.
-    if(!willAppQuit){ // avoid [Ctrl|Cmd]+Q
-      event.preventDefault();
-      switch (process.platform){
-        case 'win32':
-        console.log(willAppQuit);
-        willAppQuit = true;
-          mainWindow.minimize();
-          //mainWindow.hide();
-          break;
-        case 'darwin':
-        console.log("OK");
-          //mainWindow.hide();
-          break;
-        default:
-      }
-    }
-  });
-});
-
-app.on('before-quit', function(){
-  willAppQuit = true;
-});
-
-
-*/
-
-
-
-
 'use strict';
 
 var app = require('app');
@@ -61,7 +13,7 @@ app.on('window-all-closed', function() {
 	});
 
 app.on('ready', function() {
-		mainWindow = new BrowserWindow({width: 1800, height: 1200});
+		mainWindow = new BrowserWindow({width: 1000, height: 1000});
 		mainWindow.loadUrl('file://' + __dirname + '/index.html');
 		mainWindow.on('closed', function() {
 				mainWindow = null;
